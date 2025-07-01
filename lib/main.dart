b7ed3a1e72be171f71c32d9ea1fb3e190bbd3abe
INsevigCia <insevigdatabase1@gmail.com>
2025-07-01 08:35:16 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart'; // ✅ SOLO DESCOMENTA ESTA LÍNEA
 import 'config/supabase_config.dart';
 import 'screens/empleados_screen.dart';
 
